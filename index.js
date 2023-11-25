3ab6483b53f651874010a5278fac8ba7fa245f49
Shahid <kindlymailme@gmail.com>
2023-11-25 17:53:00 +0530

    change

--- index.js
+++ index.js
@@ -56,10 +56,10 @@ app.use("/api/v1/container-balance", contBalRouter);
 app.use("/api/v1/message-bird", messageBirdRouter);
 
 // Serve frontend folder
-app.use(express.static(path.join(__dirname, "frontend", "build")));
+app.use(express.static(path.join(__dirname, "frontend", "public")));
 // On other request paths, render static files
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "public", "index.html"));
 });
 
 app.listen(4000, () => {
